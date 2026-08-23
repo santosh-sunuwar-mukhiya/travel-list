b38bc0a71807eac48dea4ef6fc583854c205efa5
santoshsunuwarmukhiya <santoshsunuwarmukhiya@gmail.com>
2026-08-23 18:07:23 +0545

    Travel list inital setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </>
   );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>
+}
+
+function Form() {
+  return <h1>We are Far</h1>
+}
+
+function PackingList() {
+  return <h1>We are Far</h1>
+}
+
+function Stats() {
+  return <h1>We are Far</h1>
 }
 
 export default App;
